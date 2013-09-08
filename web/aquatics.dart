9d500c0df70b609ac06f4429114542cd559f5828
Antonin Stefanutti <antonin@stefanutti.fr>
2013-09-08 23:04:51 +0200

    Improve init parameters to center initial vectors

--- web/aquatics.dart
+++ web/aquatics.dart
@@ -14,16 +14,17 @@ void main() {
   var renderLoop = new RenderLoop();
   renderLoop.addStage(stage);
 
-  var padding = 150;
-  var speed = 0.009;
+  var padding = 200;
+  var speed = 0.008;
   var scale = 0.05;
   var random = new Random();
-  for (int i = 0; i < 22; i++) {
-    var x0 = padding + random.nextDouble() * (stage.stageWidth - padding);
-    var y0 = padding + random.nextDouble() * (stage.stageHeight - padding);
-    scale += 0.025;
+  for (int i = 0; i < 25; i++) {
+    var a0 = 360 * random.nextDouble();
+    var x0 = padding + random.nextDouble() * (stage.stageWidth - 2 * padding);
+    var y0 = padding + random.nextDouble() * (stage.stageHeight - 2 * padding);
+    scale += 0.022;
     speed += 0.0013;
-    var path = new RandomPath(x: x0, y: y0,
+    var path = new RandomPath(x: x0, y: y0, a0: a0,
         top:50, bottom:stage.stageHeight - 50, left:50, right:stage.stageWidth - 50,
         scale: scale, speed: speed);
     stage.addChild(path);
